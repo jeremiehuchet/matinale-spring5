d24e2d89aeebaa2f296cda52523a8e2702f07c12
Jeremie Huchet <jeremiehuchet@users.noreply.github.com>
2017-11-05 22:46:04 +0100

    add cli-runner and disable autoconfiguration

--- search-engine/src/main/java/com/point/searchengine/SearchEngineApplication.java
+++ search-engine/src/main/java/com/point/searchengine/SearchEngineApplication.java
@@ -1,12 +1,29 @@
 package com.point.searchengine;
 
+import com.point.searchengine.model.GithubProject;
+import com.point.searchengine.repository.GithubRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-public class SearchEngineApplication {
+@AllArgsConstructor
+@SpringBootConfiguration
+@ComponentScan
+public class SearchEngineApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SearchEngineApplication.class, args);
-	}
+    public final GithubRepository github;
+
+    @Override
+    public void run(String... args) throws Exception {
+        github.search("springframework").stream()
+                .map(GithubProject::getFullName)
+                .forEach(name -> System.out.println(name));
+
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchEngineApplication.class, args);
+    }
 }
