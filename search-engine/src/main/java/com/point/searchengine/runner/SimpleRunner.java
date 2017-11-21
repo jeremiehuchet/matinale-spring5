6e895e9facd1a4b22b24fd42ac8818676cf2c6d5
Jeremie Huchet <jeremiehuchet@users.noreply.github.com>
2017-11-21 22:07:04 +0100

    add metrics to simplerunner with slf4j profiler

--- search-engine/src/main/java/com/point/searchengine/runner/SimpleRunner.java
+++ search-engine/src/main/java/com/point/searchengine/runner/SimpleRunner.java
@@ -3,32 +3,41 @@ package com.point.searchengine.runner;
 import com.point.searchengine.model.GithubProject;
 import com.point.searchengine.repository.GithubRepository;
 import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.profiler.Profiler;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @Profile("simple")
 @AllArgsConstructor
 public class SimpleRunner implements CommandLineRunner {
-
-    private static final Logger log = LoggerFactory.getLogger(SimpleRunner.class);
 
     public final GithubRepository github;
     public final GithubRepository gitlab;
 
     @Override
     public void run(String... args) throws Exception {
+        Profiler p = new Profiler("search");
+        p.setLogger(log);
+
+        p.start("github");
+
         System.out.println("\nGithub results:");
         github.search("springframework").stream()
                 .map(GithubProject::getFullName)
                 .forEach(name -> System.out.println(name));
 
+        p.start("gitlab");
+
         System.out.println("\nGitlab results:");
         gitlab.search("springframework").stream()
                 .map(GithubProject::getFullName)
                 .forEach(name -> System.out.println(name));
+
+        p.stop()
+                .log();
     }
 }
